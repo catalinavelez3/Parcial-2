c46d189fdae49d98a781d29db1bc6b4e916f3a68
catalinavelez3 <98657352+catalinavelez3@users.noreply.github.com>
2023-05-19 12:14:06 +0000

    hola

---
+++ src/app/partido/partido.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PartidoComponent } from './partido.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PartidoComponent]
+})
+export class PartidoModule { }
